1ec6308e63be14c80211fcea4429900ceaf03849
JimmyCuffari <36243899+JimmyCuffari@users.noreply.github.com>
2021-03-24 19:37:07 -0400

    PathABlue incomplete

---
+++ 2021/2020_2021_/src/main/java/frc/robot/commands/PathABlue.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import frc.robot.subsystems.BeaverTailSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.FlywheelSubsystem;
+import frc.robot.subsystems.NavXTest;
+import frc.robot.subsystems.Pnumatics;
+
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+
+public class PathABlue extends SequentialCommandGroup {
+  /**
+   * Creates a new ComplexAuto.
+   * 
+   * @param DriveSubsystem
+   * @param NavXTest
+   * 
+   */
+  //double quarterTurn = 45; //45 degrees //0.5
+  double startTime = 0;
+  int ignore = 0; //this is to bypass deadzone
+
+  double rotSpeed = 0.05;
+
+  public PathABlue(DriveSubsystem m_driveCommand, NavXTest m_navX, BeaverTailSubsystem m_intake, Pnumatics m_pnumatics) {
+
+    addCommands(
+        new SolenoidOn(m_pnumatics),
+        new IntakeOn(m_intake),
+
+        new AutoDrive(m_driveCommand, m_navX,  0, 0  , 0.1, AutoDrive.Sensors.GYRO_RIGHT, 1, true), //2
+        new AutoDrive(m_driveCommand, m_navX,  0, 0.5, 0  , AutoDrive.Sensors.ENCODER, 14, false),    
+        new AutoDrive(m_driveCommand, m_navX,  0, 0  , -0.3, AutoDrive.Sensors.GYRO_LEFT, -23, false), //2
+        new AutoDrive(m_driveCommand, m_navX,  0, 0.6, 0  , AutoDrive.Sensors.ENCODER, 7, false),     
+        new AutoDrive(m_driveCommand, m_navX,  0, 0  , 0.3, AutoDrive.Sensors.GYRO_RIGHT, -8, false), //2
+        new AutoDrive(m_driveCommand, m_navX,  0, 0.6, 0  , AutoDrive.Sensors.ENCODER, 6, false),     
+        new AutoDrive(m_driveCommand, m_navX,  0, 0  , -0.3, AutoDrive.Sensors.GYRO_LEFT, 2, false), //2
+        new AutoDrive(m_driveCommand, m_navX,  0, 0.6, 0  , AutoDrive.Sensors.ENCODER, 3, false),     
+        new AutoDrive(m_driveCommand, m_navX,  0, 0.3, 0  , AutoDrive.Sensors.ENCODER, 3, false),     
+        new IntakeOff(m_intake)
+
+        );          
+        
+  }
+}
